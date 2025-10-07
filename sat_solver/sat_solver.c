89a566fa3362947741c2d17d76b2c7a0775e40ac
Ian Carlos Cidreira Tenório <iancarlosct@gmail.com>
2025-10-07 18:39:31 +0000

    Organizado

--- sat_solver/sat_solver.c
+++ sat_solver/sat_solver.c
@@ -3,35 +3,29 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_COMMENT_SIZE 1000 // Tamanho máximo de um comentário
-#define MAX_IP 100            // Quantidade máxima de variáveis
+#define MAX_COMMENT_SIZE 1000 // Maximum size of a comment line
+#define MAX_IP 100            // Maximum size of the interpretation partial list
 
-typedef struct node
-{
-    // NÓ DA ÁRVORE
-
-    int var_index;       // Índice da variável do nó na lista de interpretação parcial
-    bool value;          // Valor atribuído a variável desse nesse nó (true ou false)
-    struct node *left;
-    struct node *right;
+typedef struct node{
+    //Tree Node
+    int var_index;      // Index of the variable in the interpretation partial list
+    bool value;         // Boolean value assigned to the variable 
+    struct node *left;  // Pointer to the left child (true branch)
+    struct node *right; // Pointer to the right child (false branch)
 } node;
 
-typedef struct tree
-{
-    // HEADER DA ÁRVORE
-
-    int var_amount;      // Quantidade de variáveis da fórmula  
-    node *root;          // Raiz da árvore (não guarda uma variável)
-    int interp[MAX_IP];  // Lista de interpretação parcial
+typedef struct tree{
+    //Tree Header
+    int var_amount;      // Amount of variables in the formula
+    node *root;          // Pointer to the root node of the tree
+    int interp[MAX_IP];  // List of the interpretation partial (1 = true, -1 = false, 0 = unassigned)
 } tree;
 
-node *create_node(int var_index, bool value)
-{
-    // FUNÇÃO -> CRIA UM NOVO NÓ
-    // var_index -> índice da variável do nó na lista de interpretação parcial
-    // value -> valor booleano a ser atribuído a variável do nó
-    // RETORNA -> NOVO NÓ
-
+node *create_node(int var_index, bool value){
+    //Function -> Create a new node
+    // @param var_index -> index of the variable in the interpretation partial list
+    // @param value -> boolean value to be assigned to the variable
+    // @return -> new node
     node *new_node = (node*) malloc(sizeof(node));
     new_node->var_index = var_index;
     new_node->value = value;
@@ -40,26 +34,20 @@ node *create_node(int var_index, bool value)
     return new_node;
 }
 
-node *add_node(tree *header, node *curr, int var_index, bool value)
-{
-    // FUNÇÃO -> ADICIONAR UM NOVO NÓ NA ÁRVORE
-    // header -> cabeçalho da árvore
-    // curr -> nó atual, onde será adicionado um novo nó no left ou right
-    // var_index -> índice da variável do nó na lista de interpretação parcial
-    // value -> valor booleano a ser atribuído a variável do nó
-    // RETORNA -> NOVO NÓ
-
+node *add_node(tree *header, node *curr, int var_index, bool value){
+    //Function -> Add a new node to the tree
+    // @param header -> tree header
+    // @param curr -> current node where the new node will be added as left or right
+    // @param var_index -> index of the variable in the interpretation partial list
+    // @param value -> boolean value to be assigned to the variable
+    // @return -> new node added to the tree
     node *new_node = create_node(var_index, value);
     if(value) header->interp[var_index] = 1;
     else header->interp[var_index] = -1;
-
-    if(curr == NULL)
-    {
+    if(curr == NULL){
         if(header->root->left == NULL) header->root->left = new_node;           
         else header->root->right = new_node;
-    }
-    else
-    {
+    } else{
         if(value) curr->left = new_node;
         else curr->right = new_node;
     }
@@ -67,28 +55,22 @@ node *add_node(tree *header, node *curr, int var_index, bool value)
     return new_node;
 }
 
-bool satisfies(int cla, int lit, int formula[cla][lit], int interp[MAX_IP])
-{
-    // FUNÇÃO -> TESTAR SE A FÓRMULA COM A INTERPRETAÇÃO PARCIAL ATUAL É VERDADEIRA
-    // cla -> número de cláusulas
-    // lit -> número de variáveis (literais)
-    // formula -> matriz onde guardamos a fórmula
-    // interp -> lista da interpretação parcial
-    // RETORNA -> TRUE se a fórmula for verdadeira com a IP (SATISFATÍVEL)
-    // RETORNA -> FALSE se a fórmula for inconclusiva com a IP (INCONCLUSIVA != CONTRADIÇÃO)
-
-    for(int i = 0; i < cla; i++)
-    {
+bool satisfies(int cla, int lit, int formula[cla][lit], int interp[MAX_IP]) {
+    // Function -> Test if the formula with the current partial interpretation is true
+    // @param cla -> number of clauses
+    // @param lit -> number of variables (literals)
+    // @param formula -> matrix where the formula is stored
+    // @param interp -> list of the partial interpretation
+    // @return -> TRUE if the formula is true with the IP (SATISFIABLE)
+    // @return -> FALSE if the formula is inconclusive with the IP (INCONCLUSIVE != UNSATISFIABLE)
+    for(int i = 0; i < cla; i++) {
         bool satisfied = false;
-        for(int j = 0; j < lit; j++)
-        {
-            if(formula[i][j] == 2)
-            {
+        for(int j = 0; j < lit; j++) {
+            if(formula[i][j] == 2) {
                 satisfied = true;
                 break;
             }
-            if(formula[i][j] == interp[j] && interp[j] != 0) 
-            {
+            if(formula[i][j] == interp[j] && interp[j] != 0) {
                 satisfied = true;
                 break;
             }
@@ -100,31 +82,25 @@ bool satisfies(int cla, int lit, int formula[cla][lit], int interp[MAX_IP])
 
 bool contradicts(int cla, int lit, int formula[cla][lit], int interp[MAX_IP])
 {
-    // FUNÇÃO -> TESTAR SE A FÓRMULA COM A INTERPRETAÇÃO PARCIAL ATUAL É FALSA
-    // cla -> número de cláusulas
-    // lit -> número de variáveis (literais)
-    // formula -> matriz onde guardamos a fórmula
-    // interp -> lista da interpretação parcial
-    // RETORNA -> TRUE se a fórmula for falsa com a IP (CONTRADIÇÃO PARCIAL)
-    // RETORNA -> FALSE se a fórmula for inconclusiva com a IP (INCONCLUSIVA != SATISFATÍVEL)
-
-    for(int i = 0; i < cla; i++)
-    {
+    // Function -> Test if the formula with the current partial interpretation is false
+    // @param cla -> number of clauses
+    // @param lit -> number of variables (literals)
+    // @param formula -> matrix where the formula is stored
+    // @param interp -> list of the partial interpretation
+    // @return -> TRUE if the formula is false with the IP (CONTRADICTION)
+    // @return -> FALSE if the formula is inconclusive with the IP (INCONCLUSIVE != SATISFIABLE)
+    for(int i = 0; i < cla; i++) {
         bool contradicted = true;
-        for(int j = 0; j < lit; j++)
-        {
-            if(formula[i][j] == 2) 
-            {
+        for(int j = 0; j < lit; j++) {
+            if(formula[i][j] == 2) {
                 contradicted = false;
                 break;
             }
-            if(formula[i][j] != 0 && interp[j] == 0) 
-            {
+            if(formula[i][j] != 0 && interp[j] == 0) {
                 contradicted = false;
                 break;
             }
-            if(formula[i][j] == interp[j] && interp[j] != 0) 
-            {
+            if(formula[i][j] == interp[j] && interp[j] != 0) {
                 contradicted = false;
                 break;
             }
@@ -136,16 +112,15 @@ bool contradicts(int cla, int lit, int formula[cla][lit], int interp[MAX_IP])
 
 bool sat(int cla, int lit, int formula[cla][lit], tree *header, node *curr, int var_index)
 { 
-    // FUNÇÃO -> REALIZAR A LÓGICA DE RESOLUÇÃO DO SAT
-    // cla -> número de cláusulas
-    // lit -> número de variáveis (literais)
-    // formula -> matriz onde guardamos a fórmula
-    // header -> cabeçalho da árvore
-    // curr -> nó atual, onde será adicionado um novo nó no left ou right
-    // var_index -> índice da variável do nó na lista de interpretação parcial
-    // RETORNA -> TRUE se a fórmula for SATISFATÍVEL
-    // RETORNA -> FALSE se a fórmula for CONTRADIÇÃO
-
+    // Function -> Recursive function that builds the binary tree and tests the formula with the current partial interpretation
+    // @param cla -> number of clauses
+    // @param lit -> number of variables (literals)
+    // @param formula -> matrix where the formula is stored
+    // @param header -> tree header
+    // @param curr -> current node where the new node will be added as left or right
+    // @param var_index -> index of the variable in the interpretation partial list
+    // @return -> TRUE if the formula is SATISFIABLE
+    // @return -> FALSE if the formula is UNSATISFIABLE
     if(satisfies(cla, lit, formula, header->interp)) return true;
     if(contradicts(cla, lit, formula, header->interp)) return false;
 
@@ -157,17 +132,16 @@ bool sat(int cla, int lit, int formula[cla][lit], tree *header, node *curr, int 
     node *node_f = add_node(header, curr, var_index, false);
     if(sat(cla, lit, formula, header, node_f, var_index+1)) return true;
     
-    header->interp[var_index] = 0;
+    header->interp[var_index] = 0; // Backtrack: unassign the variable
 
     return false;
 }
 
 tree *initialize_tree(int var_amount)
 {
-    // FUNÇÃO -> INICIALIZAR A ÁRVORE (CABEÇALHO)
-    // var_amount -> quantidade de variáveis da fórmula
-    // RETORNO -> NOVA ÁRVORE (CABEÇALHO)
-
+    // Function -> Initialize the tree header
+    // @param var_amount -> amount of variables in the formula
+    // @return -> new tree header
     tree *new_tree = (tree*) malloc(sizeof(tree));
     new_tree->root = (node*) malloc(sizeof(node));
     new_tree->root->var_index = -1;
@@ -182,37 +156,31 @@ tree *initialize_tree(int var_amount)
 
 int main()
 {  
-    char cmd;                           // Guarda o Char que determina se a linha é comentário (c) ou início da fórmula (p)
-    char comment[MAX_COMMENT_SIZE];     // Guarda o Comentário atual conforme lê os comentários
-    char form[10];                      // Guarda o tipo de fórmula de entrada (Tem que ser CNF)
-    int lit, cla;                       // Guarda a quantidade de Variáveis (lit) e Cláusulas (cla)
+    char cmd;                           // Command Character to identify the type of line being read
+    char comment[MAX_COMMENT_SIZE];     // Keeps the comment line
+    char form[10];                      // Keeps the format of the formula (should be "cnf")
+    int lit, cla;                       // Amount of literals and clauses
     
 
-    // REALIZA A LEITURA DO CABEÇALHO DO ARQUIVO DE ENTRADA
-    while(scanf(" %c", &cmd) != EOF)
-    {
-        if(cmd == 'c')
-        {
+    // Reads the input until it finds the "p cnf" line
+    while(scanf(" %c", &cmd) != EOF) {
+        if(cmd == 'c') {
             fgets(comment, sizeof(comment), stdin);
             comment[strlen(comment)-1] = '\0';
             printf("=== %s ===\n", comment+1);
         }
-        if(cmd == 'p')
-        {
+        if(cmd == 'p'){
             scanf("%s", form);
-            if(strcmp(form, "cnf") != 0) 
-            {
+            if(strcmp(form, "cnf") != 0) {
                 printf("Invalid Format, Please Use CNF Format\n");
                 return 0;
             }
             scanf("%d %d", &lit, &cla);
-            if(lit <= 0)
-            {
+            if(lit <= 0) {
                 printf("Literals Number Invalid, Please Use a Valid Amount\n");
                 return 0;
             }
-            if(cla <= 0)
-            {
+            if(cla <= 0){
                 printf("Clauses Number Invalid, Please Use a Valid Amount\n");
                 return 0;
             }
@@ -221,32 +189,23 @@ int main()
     }
     printf("\n");
 
-    
-    int formula[cla][lit];                  // Matriz onde a fórmula é guardada
-    memset(formula, 0, sizeof(formula));    // Zera todos os elementos da matriz
+    int formula[cla][lit];                  // Matrix to store the formula
+    memset(formula, 0, sizeof(formula));    // Initialize the matrix with 0 (no literal in the clause)
 
-    // REALIZA A LEITURA DA FÓRMULA E ELIMINA AS CLÁUSULAS COM TAUTOLOGIA
-    for(int i = 0; i < cla; i++)
-    {
+    // Reads the formula and fills the matrix
+    for(int i = 0; i < cla; i++) {
         int aux, tautology = 0;
-    
-        while(1)
-        {
+        while(1){
             scanf("%d", &aux);
 
-            if(aux == 0) break; // Fim da cláusula
-
-            if(aux > 0) 
-            {
-                if (formula[i][aux-1] == -1) 
-                {
+            if(aux == 0) break; // End of clause
+            if(aux > 0) {
+                if (formula[i][aux-1] == -1) {
                     tautology = 1;
                 }
                 formula[i][aux-1] = 1;
             } 
-
-            if(aux < 0) 
-            {
+            if(aux < 0) {
                 if (formula[i][-aux-1] == 1) 
                 {
                     tautology = 1;
@@ -254,48 +213,39 @@ int main()
                 formula[i][-aux-1] = -1;
             }
         }   
-
-        // Em caso de tautologia da cláusula: (A || ~A) -> Sempre TRUE
-        if(tautology)
-        {
-            memset(formula[i], 0, lit * sizeof(int)); // Zera a cláusula
-            formula[i][0] = 2;                        // Primeiro elemento da cláusula recebe 2 para identificar as cláusulas com tautologia na função satisfies() e contradicts()
+        // In case of tautology, the clause is set to a special value (2) to be ignored in satisfies() and contradicts()
+        if(tautology) {
+            memset(formula[i], 0, lit * sizeof(int)); // Clear the clause
+            formula[i][0] = 2;                        // Set the first position to 2 to indicate tautology
         }
     }
 
-    // PRINT DA FÓRMULA NA MATRIZ
+    // Print the formula stored in memory
     printf("Fórmula na Memória:\n");
-    for(int i = 0; i < cla; i++)
-    {
-        for(int j = 0; j < lit; j++)
-        {
+    for(int i = 0; i < cla; i++) {
+        for(int j = 0; j < lit; j++){
             printf("%d ", formula[i][j]);
         }
         printf("\n");
     }
     printf("\n");
 
-    // INICIALIZA A ÁRVORE (CABEÇALHO)
+    // Initialize the tree header
     tree *header = initialize_tree(lit);
     
-    // CHAMA SAT (RES = TRUE para SATISFATÍVEL) && (RES == FALSE para CONTRADIÇÃO) 
+    // Call the SAT function
     bool res = sat(cla, lit, formula, header, header->root, 0);
 
-    // PRINT DO RESULTADO E DA INTERPRETAÇÃO PARCIAL EM CASO DE FÓRMULA SATISFATÍVEL
-    if(res) 
-    {
+    // Print the result
+    if(res) {
         printf("SATISFIABLE\n");
-        for(int i = 0; i < MAX_IP; i++)
-        {
-            if(header->interp[i] != 0)
-            {
+        for(int i = 0; i < MAX_IP; i++){
+            if(header->interp[i] != 0){
                 printf("X%d: %d ", i+1, header->interp[i]);
             }
         }
         printf("\n");
-    } 
-    else
-    {
+    } else{
         printf("UNSATISFIABLE\n");
     }
 
